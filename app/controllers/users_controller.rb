273b59e58d03b73fc0d8f930e8bd1b63c1cb3518
Steen Clarke <cmclarkey@gmail.com>
2016-08-29 10:52:40 -0700

    Make create and show actions for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.new(
+      name: params[:name], 
+      email: params[:email], 
+      password: params[:password], 
+      password_confirmation: params[:password_confirmation]
+      )
+    if user.save
+      redirect_to "/users/#{user.id}"
+    else
+      redirect_to '/signup'
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @posts = Post.where(user_id: @user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'posts#index'
+  
   get '/login', to: 'sessions#new'
   get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
 end
