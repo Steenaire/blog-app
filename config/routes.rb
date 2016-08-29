dab8fc91d546a9feb5caf64c2eb4a30cd594bf84
Steen Clarke <cmclarkey@gmail.com>
2016-08-29 11:02:59 -0700

    Make current_user helper method, make log in/log out/sign up links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get '/', to: 'posts#index'
-  
+
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
 end
