67d14c880cb8b6125839da747b8d93326a531d8d
Steen Clarke <cmclarkey@gmail.com>
2016-08-29 14:14:40 -0700

    Replace text_field_tag with collection_select for category selection

--- app/controllers/post_categories_controller.rb
+++ app/controllers/post_categories_controller.rb
@@ -1,6 +1,6 @@
 class PostCategoriesController < ApplicationController
   def create
-    post_category = PostCategory.new(post_id: params[:post_id], category_id: params[:category_id])
+    post_category = PostCategory.new(post_id: params[:post_id], category_id: params[:category][:category_id])
     if post_category.save
       redirect_to "/users/#{current_user.id}"
     end
